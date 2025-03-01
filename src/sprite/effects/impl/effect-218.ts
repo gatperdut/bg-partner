c536406f1ca84ad4719fd69cb65fa9fc3f3a0d2b
Carlos Romero <clear>
2025-03-01 10:54:36 +0100

    effect 218 + params

---
+++ src/sprite/effects/impl/effect-218.ts
@@ -0,0 +1,12 @@
+import { Effect } from './effect';
+
+// Protection_Stoneskin
+export class Effect218 extends Effect {
+  constructor(public id: number, protected base: bigint) {
+    super(id, base);
+  }
+
+  public summary(): void {
+    super.summary();
+  }
+}
--- src/sprite/effects/impl/effect.ts
+++ src/sprite/effects/impl/effect.ts
@@ -1,11 +1,34 @@
+import { handlers } from '../../../handlers';
 import { effectTable } from '../../../tables/effect';
 
 export abstract class Effect {
+  public param1: number;
+
+  public param2: number;
+
+  public param3: number;
+
+  public param4: number;
+
+  public param5: number;
+
   constructor(public id: number, protected base: bigint) {
-    // Empty
+    this.param1 = handlers.memread.memReadNumber(base + BigInt(0x8 + 0x14), 'INT32');
+
+    this.param2 = handlers.memread.memReadNumber(base + BigInt(0x8 + 0x58), 'INT32');
+
+    this.param3 = handlers.memread.memReadNumber(base + BigInt(0x8 + 0x5c), 'INT32');
+
+    this.param4 = handlers.memread.memReadNumber(base + BigInt(0x8 + 0x60), 'INT32');
+
+    this.param5 = handlers.memread.memReadNumber(base + BigInt(0x8 + 0x64), 'INT32');
   }
 
   public summary(): void {
-    console.log(effectTable[this.id]);
+    console.log(
+      `${effectTable[this.id]} ${this.param1} ${this.param2} ${this.param3} ${this.param4} ${
+        this.param5
+      }`
+    );
   }
 }
