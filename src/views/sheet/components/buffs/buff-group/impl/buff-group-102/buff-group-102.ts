5801e998422bcc3d2e6fde763daf1beb4366c42c
Carlos Romero <clear>
2025-03-12 16:28:34 +0100

    work on buff-right

--- src/views/sheet/components/buffs/buff-group/impl/buff-group-102/buff-group-102.ts
+++ src/views/sheet/components/buffs/buff-group/impl/buff-group-102/buff-group-102.ts
@@ -4,10 +4,11 @@ import { ComponentsRecord } from '../../../../../../../components/components';
 import { Eff } from '../../../../../../../sprite/effs/impl/eff';
 import { SheetAPIUpdateParams } from '../../../../../../../views/sheet/renderer';
 import { BuffImage } from '../../../parts/buff-image/buff-image';
+import { BuffRight } from '../../../parts/buff-right/buff-right';
 import { BuffGroup, BuffGroupData } from '../../buff-group';
 
 export type BuffGroup102Data = BuffGroupData & {
-  max: number;
+  rightHtml: string;
 };
 
 export class BuffGroup102 extends BuffGroup {
@@ -20,10 +21,12 @@ export class BuffGroup102 extends BuffGroup {
 
     const compiled: HandlebarsTemplateDelegate = Handlebars.compile(this.components.buffGroup102);
 
+    const max: number = Math.max(..._.map(this.effs, (eff: Eff): number => eff.param1));
+
     const buffGroup102Data: BuffGroup102Data = {
       ...this.buffData,
       imageHtml: new BuffImage(components, effs[0], null).html,
-      max: Math.max(..._.map(this.effs, (eff: Eff): number => eff.param1)),
+      rightHtml: new BuffRight(components, max, 'Maximum level').html,
     };
 
     this.html = compiled(buffGroup102Data);
--- src/views/sheet/components/buffs/parts/buff-right/buff-right.ts
+++ src/views/sheet/components/buffs/parts/buff-right/buff-right.ts
@@ -3,16 +3,19 @@ import { ComponentsRecord } from '../../../../../../components/components';
 
 export type BuffRightData = {
   value: number;
+
+  tooltip: string;
 };
 
-export class BuffLevels {
+export class BuffRight {
   public html: string;
 
-  constructor(components: ComponentsRecord, value: number) {
+  constructor(components: ComponentsRecord, value: number, tooltip: string) {
     const compiled: HandlebarsTemplateDelegate = Handlebars.compile(components.buffRight);
 
     const buffRightData: BuffRightData = {
       value: value,
+      tooltip: tooltip,
     };
 
     this.html = compiled(buffRightData);
