1ad2d2dd8a882724571841478845060718e31bd6
Carlos Romero <clear>
2025-03-21 04:38:36 +0100

    meh

--- src/sprite/sprite.ts
+++ src/sprite/sprite.ts
@@ -9,37 +9,20 @@ export class Sprite {
 
   public profile: Profile;
 
-  // TODO do we need any besides derivedBonus?
   public derived: Derived;
 
-  // public derivedBonus: Derived;
-
-  // public derivedTemp: Derived;
-
   private derivedOffset: bigint;
-
-  private derivedTempOffset: bigint;
-
-  private derivedBonusOffset: bigint;
 
   public effs: Effs;
 
   constructor(public base: bigint) {
     this.derivedOffset = base + BigInt(0x1120);
 
-    this.derivedTempOffset = base + BigInt(0x1dc8 - (handlers.linux ? 0x8 : 0x0));
-
-    this.derivedBonusOffset = base + BigInt(0x2a70 - (handlers.linux ? 0x10 : 0x0));
-
     this.basic = new Basic(base);
 
     this.profile = new Profile(base, this.derivedOffset);
 
     this.derived = new Derived(this.derivedOffset);
-
-    // this.derivedTemp = new Derived(this.derivedTempOffset);
-
-    // this.derivedBonus = new Derived(this.derivedBonusOffset);
 
     this.effs = new Effs(
       base + BigInt(0x4a00 - (handlers.linux ? 0x18 : 0)),
@@ -65,10 +48,6 @@ export class Sprite {
     this.profile.run();
 
     this.derived.run();
-
-    // this.derivedBonus.run();
-
-    // this.derivedTemp.run();
 
     this.effs.run();
   }
--- src/views/sheet/sheet.ts
+++ src/views/sheet/sheet.ts
@@ -101,6 +101,7 @@ export class Sheet {
       spriteView: spriteView(this.sprite),
     };
 
+    console.log(this.sprite.basic.name, this.sprite.base);
     this.window.webContents.send('sheet.update', params);
   }
 
